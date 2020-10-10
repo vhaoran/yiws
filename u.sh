0686442d59c419a3838a28147dd1de914e851745
whr <whr@whr.local>
2020-10-10 10:43:20 +0800

    aaaa

--- u.sh
+++ u.sh
@@ -2,7 +2,8 @@ rm -rf *.out
 echo "making....."
 echo $(pwd)
 
-CC_x86_64_unknown_linux_musl="x86_64-linux-musl-gcc" cargo build --release --target=x86_64-unknown-linux-musl
+# CC_x86_64_unknown_linux_musl="x86_64-linux-musl-gcc" 
+cargo build --release --target=x86_64-unknown-linux-musl
 
 cp ./target/release/yi-ws  ./yi-ws.out
 # upx -9 *.out

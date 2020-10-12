6cef15a06e06783c931b460bb5cefb5a7524aea7
whr <whr@whr.local>
2020-10-12 17:27:03 +0800

    aaaa

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ extern crate env_logger;
 extern crate ws;
 extern crate log;
 
-
 use yi_ws::msg_util::rx_tx::prepare_rtx;
 use yi_ws::router_util::router::{Router, NotFound};
 use yi_ws::config_file::config_init::{init_cfg, get_cfg_port};
@@ -12,6 +11,8 @@ use yi_ws::log_util::init_log;
 use log::*;
 
 mod msg_util;
+
+use ws::{Builder, Sender, Settings};
 
 fn main() {
     //env_logger::init();
@@ -26,16 +27,20 @@ fn main() {
 
 
     // Listen on an address and call the closure for each connection
-    if let Err(error) = ws::listen(cnt, |out| {
-        // Use our router as the handler to route the new connection
-        Router {
-            sender: out,
-            // Default to returning a 404 when the route doesn't match.
-            // You could default to any handler here.
-            inner: Box::new(NotFound),
-        }
-    }) {
-        // Inform the user of failure
+    if let Err(error) =
+    Builder::new()
+        .with_settings(Settings {
+            max_connections: 10_0000,
+            ..Settings::default()
+        })
+        .build(|out| {
+            Router {
+                sender: out,
+                inner: Box::new(NotFound),
+            }
+        })
+        .unwrap()
+        .listen(cnt) {
         error!("Failed to create WebSocket due to {:?}", error);
     }
 }

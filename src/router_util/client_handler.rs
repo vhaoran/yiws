183e2071329c6796bff7a08dab2293b62aa3cbd3
whr <whr@whr.local>
2020-10-14 15:37:32 +0800

    aaaa

--- src/router_util/client_handler.rs
+++ src/router_util/client_handler.rs
@@ -4,7 +4,7 @@ extern crate log;
 
 use log::*;
 use crate::msg_util::cnt;
-use self::ws::CloseCode;
+// use self::ws::CloseCode;
 
 pub struct ClientHandler {
     pub ws: ws::Sender,
@@ -31,15 +31,13 @@ impl ws::Handler for ClientHandler {
         Ok(())
     }
 
-    fn on_close(&mut self, code: ws::CloseCode, _reason: &str) {
+    fn on_close(&mut self, _code: ws::CloseCode, _reason: &str) {
         cnt::rm_cnt(self.uid);
-        info!(" on_close: {}", self.uid)
-        self.ws.close(code);
+        info!(" on_close: {}", self.uid);
     }
 
     fn on_error(&mut self, _err: ws::Error) {
         cnt::rm_cnt(self.uid);
-        self.ws.close(CloseCode::Normal);
         error!(" on_error: {}", self.uid)
     }
 
